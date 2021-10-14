4067d66f14888c92140f28ffae008124bf015070
Restyled.io <commits@restyled.io>
2021-10-14 15:47:12 +0000

    Restyled by isort

--- services/server/src/main.py
+++ services/server/src/main.py
@@ -1,9 +1,8 @@
-from fastapi import FastAPI
-from services.server.src.subscribe.webhook import webhook
-from services.server.src.subscribe.email import email
-
 import uvicorn
+from fastapi import FastAPI
 
+from services.server.src.subscribe.email import email
+from services.server.src.subscribe.webhook import webhook
 
 app = FastAPI()
 app.include_router(webhook)
