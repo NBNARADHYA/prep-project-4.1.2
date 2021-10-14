a6043a7990ac5c565ab0a1f82f86efa7cb8cdb27
Mazen Sayed <54520113+Mazensayed91@users.noreply.github.com>
2021-10-14 13:16:43 +0200

    added email notification

---
+++ services/server/src/dependencies.py
@@ -0,0 +1,10 @@
+from fastapi import HTTPException, Header, Depends
+
+
+async def verify_token(x_token: str = Header(...)):
+    return True
+
+
+async def verify_token(token: str = Header("Authorization")):
+    if not verify_token(token):
+        raise HTTPException(status_code=401, detail="Unauthorized token.")
--- services/server/src/main.py
+++ services/server/src/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
-from subscribe.webhook import webhook
+from services.server.src.subscribe.webhook import webhook
+from services.server.src.subscribe.email import email
+
+import uvicorn
+
 
 app = FastAPI()
 app.include_router(webhook)
+app.include_router(email)
