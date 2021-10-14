4c3fbc9ed333bff0e1f0eed5f5b0c1a7fa7d8bed
Restyled.io <commits@restyled.io>
2021-10-14 15:47:08 +0000

    Restyled by black

--- services/server/src/subscribe/webhook.py
+++ services/server/src/subscribe/webhook.py
@@ -1,22 +1,20 @@
 from fastapi import APIRouter
 import re
 
-webhook = APIRouter(
-    tags=["webhook"]
-)
+webhook = APIRouter(tags=["webhook"])
 
 
-@webhook.post('/api/subscribe/webhook')
+@webhook.post("/api/subscribe/webhook")
 # add authentication filter here.
 async def webhook_subscribe(webhook_url: str):
     print(webhook_url)
 
     webhook_regex = re.compile(
-        "https:\/\/(www\.|)(discord|discordapp)\.com\/api\/webhooks\/([\d]{18})\/([a-z0-9_-]+)")
+        "https:\/\/(www\.|)(discord|discordapp)\.com\/api\/webhooks\/([\d]{18})\/([a-z0-9_-]+)"
+    )
     filtering = webhook_regex.match(webhook_url)
     print(filtering, bool(filtering))
     if not bool(filtering):
-        return {"status": "failed",
-                "reason": "incorrect discord webhook."}
+        return {"status": "failed", "reason": "incorrect discord webhook."}
 
     return {"status": "dummy"}
