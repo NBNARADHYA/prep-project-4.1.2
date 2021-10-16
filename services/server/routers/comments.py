3284accddd5f062c181f8d6aa4f11386620855e0
Restyled.io <commits@restyled.io>
2021-10-16 18:19:07 +0000

    Restyled by black

--- services/server/routers/comments.py
+++ services/server/routers/comments.py
@@ -14,26 +14,24 @@ from starlette.responses import JSONResponse
 
 from services.server.schemas import Comment
 
-comment = APIRouter(
-    tags=["comment"]
-)
+comment = APIRouter(tags=["comment"])
 oauth = OAuth(starlette_config)
 
 
-@comment.post('/api/comments')
+@comment.post("/api/comments")
 async def email_subscribe(req: Request, db: Session = Depends(get_db)):
     try:
         access_token = await oauth.google.authorize_access_token(req)
     except OAuthError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail='Could not validate credentials',
-            headers={'WWW-Authenticate': 'Bearer'},
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
         )
     user_data = await oauth.google.parse_id_token(req, access_token)
-    email = user_data['email']
-    user_comment = user_data['comment']
-    user_id = user_data['id']
+    email = user_data["email"]
+    user_comment = user_data["comment"]
+    user_id = user_data["id"]
     item = Comment(user_comment, email)
 
     return crud.create_user_comment(db=db, item=item, user_id=user_id)
