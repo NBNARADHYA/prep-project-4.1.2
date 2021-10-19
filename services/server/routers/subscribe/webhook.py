e4b9d42fffeeae2e44b54e9ff20480ebd2a4efe6
Restyled.io <commits@restyled.io>
2021-10-19 18:28:17 +0000

    Restyled by autopep8

--- services/server/routers/subscribe/webhook.py
+++ services/server/routers/subscribe/webhook.py
@@ -6,28 +6,30 @@ from db.crud import get_db
 import re
 
 router = APIRouter(
-   tags = ["webhook"]
+    tags=["webhook"]
 )
 
-@router.post('/subscribe/webhook', response_model = User)
-async def webhook_subscribe( webhook_url : str, trigger_name : str, location : str, user: User = Depends(get_current_user)):
 
-    webhook_regex = re.compile("https:\/\/(www\.|)(discord|discordapp)\.com\/api\/webhooks\/([\d]{18})\/([a-z0-9_-]+)")
+@router.post('/subscribe/webhook', response_model=User)
+async def webhook_subscribe(webhook_url: str, trigger_name: str, location: str, user: User = Depends(get_current_user)):
+
+    webhook_regex = re.compile(
+        "https:\/\/(www\.|)(discord|discordapp)\.com\/api\/webhooks\/([\d]{18})\/([a-z0-9_-]+)")
     filtering = webhook_regex.match(webhook_url)
 
     if not bool(filtering):
-        return {"status" : "failed",
-                "reason" : "incorrect discord webhook."}
+        return {"status": "failed",
+                "reason": "incorrect discord webhook."}
 
     db = get_db()
 
-    entry = models.Webhook(trigger_name = trigger_name, 
-    url = webhook_url, 
-    user_email = user.email, 
-    type = "webhook", 
-    place = location)
+    entry = models.Webhook(trigger_name=trigger_name,
+                           url=webhook_url,
+                           user_email=user.email,
+                           type="webhook",
+                           place=location)
 
     db.add(entry)
     db.commit()
 
-    return {"status" : "success"}
+    return {"status": "success"}
