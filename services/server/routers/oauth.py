c2cb4de0f293be73d0f6b72b61b6e1fbe2f8a6eb
Restyled.io <commits@restyled.io>
2021-10-20 11:32:55 +0000

    Restyled by black

--- services/server/routers/oauth.py
+++ services/server/routers/oauth.py
@@ -15,29 +15,29 @@ from datetime import timedelta
 router = APIRouter()
 
 
-GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID') or None
-GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET') or None
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID") or None
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET") or None
 if GOOGLE_CLIENT_ID is None or GOOGLE_CLIENT_SECRET is None:
-    raise BaseException('Missing env variables')
+    raise BaseException("Missing env variables")
 
 # Set up OAuth
-config_data = {'GOOGLE_CLIENT_ID': GOOGLE_CLIENT_ID,
-               'GOOGLE_CLIENT_SECRET': GOOGLE_CLIENT_SECRET}
+config_data = {
+    "GOOGLE_CLIENT_ID": GOOGLE_CLIENT_ID,
+    "GOOGLE_CLIENT_SECRET": GOOGLE_CLIENT_SECRET,
+}
 starlette_config = Config(environ=config_data)
 oauth = OAuth(starlette_config)
 
 
-CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+CONF_URL = "https://accounts.google.com/.well-known/openid-configuration"
 oauth.register(
-    name='google',
+    name="google",
     server_metadata_url=CONF_URL,
-    client_kwargs={
-        'scope': 'openid email profile'
-    }
+    client_kwargs={"scope": "openid email profile"},
 )
 
 # Frontend Host
-FRONTEND_HOST = os.environ.get('FRONTEND_HOST') or 'http://localhost:3000'
+FRONTEND_HOST = os.environ.get("FRONTEND_HOST") or "http://localhost:3000"
 
 # Frontend URL:
 FRONTEND_URL = "{host}/oauth2callback".format(host=FRONTEND_HOST)
@@ -49,27 +49,34 @@ async def login_redirect(req: Request):
     return await oauth.google.authorize_redirect(req, redirect_uri)
 
 
-@router.get('/oauth2callback')
+@router.get("/oauth2callback")
 async def oauth2callback(req: Request, db: Session = Depends(get_db)):
     try:
         access_token = await oauth.google.authorize_access_token(req)
     except OAuthError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail='Could not validate credentials',
-            headers={'WWW-Authenticate': 'Bearer'},
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
         )
     user_data = await oauth.google.parse_id_token(req, access_token)
-    user = crud.get_user_by_email(db, user_data['email'])
+    user = crud.get_user_by_email(db, user_data["email"])
 
     if not user:
         user = schemas.User(email=user_data.email, name=user_data.name)
         crud.create_user(db, user)
 
-    payload = {key: user_data[key] for key in [
-        'email', 'email_verified', 'name', 'picture', 'locale']}
+    payload = {
+        key: user_data[key]
+        for key in ["email", "email_verified", "name", "picture", "locale"]
+    }
 
-    return JSONResponse({'result': True, 'access_token': create_access_token(payload, timedelta(weeks=+4))})
+    return JSONResponse(
+        {
+            "result": True,
+            "access_token": create_access_token(payload, timedelta(weeks=+4)),
+        }
+    )
 
 
 # {'iss': 'https://accounts.google.com', 'azp': '153729250130-mgekntsf4mea7os4pbhga4elull61bu8.apps.googleusercontent.com',
